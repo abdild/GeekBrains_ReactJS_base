cd9f4837343f8d25c647bbb15e9fd491c3a161cf
unknown <abdullin@yandex.ru>
2021-08-07 09:12:08 +0300

    lesson_5

--- my-app/src/components/Profile/index.js
+++ my-app/src/components/Profile/index.js
@@ -1,5 +1,80 @@
-import React from 'react';
+// import React from 'react';
+// import { store } from '../../store';
+// import { PROFILE_TOGGLE_SHOW } from "../../store/actionTypes";
+
+// export const Profile = () => {
+//     // const profileState = store.getState();
+//     // console.log(profileState);
+//     const profileState = useSelector((state) => state);
+//     const dispatch = useDispatch();
+
+//     const toggleShow = () => {
+//         dispatch({
+//             type: PROFILE_TOGGLE_SHOW,
+//         });
+//     };
+//     console.log(profileState);
+
+//     return (
+//         <>
+//             <div className="page404">THIS IS PROFILE PAGES</div>
+//             <button onClick={toggleShow}>TOGGLE show</button>
+//         </>
+
+//     )
+// };
+
+import React, { useState } from "react";
+import { store } from "../../store";
+import { PROFILE_CHECK, PROFILE_TOGGLE_SHOW } from "../../store/actionTypes";
+import { useSelector, useDispatch } from "react-redux";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Typography } from "@material-ui/core";
 
 export const Profile = () => {
-    return <div className="page404">THIS IS PROFILE PAGES</div>
+    const profileState = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    const toggleShow = () => {
+        dispatch({
+            type: PROFILE_TOGGLE_SHOW,
+        });
+    };
+    
+    const handleCheckPoint = () => {
+        dispatch({
+            type: PROFILE_CHECK,
+        });
+    };
+
+    return (
+        <>
+            <div className="page404">THIS IS PROFILE PAGES</div>
+            <button onClick={toggleShow}>TOGGLE show</button><div>
+                {/* <Checkbox
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                    label={<div className="page404">On</div>}
+                /> */}
+                <FormControlLabel style={{color: 'white'}}
+                    control={
+                        <Checkbox
+                            checked={profileState.checked}
+                            onChange={handleCheckPoint}
+                            // name="checkedB"
+                            color="primary"
+                            style ={{
+                                color: "#00e676",
+                              }}
+                        />
+                    }
+                    label={<Typography variant="h6" style={{ color: '#2979ff' }}>Check this checkbox</Typography>}
+                />
+            </div>
+            {profileState.checked && <div className="page404">Check</div>}
+            {profileState.show && <div className="page404">THIS DEPENDS ON GLOBAL REDUX STATE</div>}
+        </>
+    );
 };
